203601e7c132b52b00259a2089fdf57c6fd871b6
zadremal <anton.kamynin@gmail.com>
2018-06-09 15:51:12 +0300

    Refaactor: organize mail_jet code with PEP8 style guide

--- REST/mail_jet.py
+++ REST/mail_jet.py
@@ -2,32 +2,33 @@ import os
 from mailjet_rest import Client
 
 from datetime import *
-
 API_KEY = os.environ['MJ_APIKEY_PUBLIC']
 API_SECRET = os.environ['MJ_APIKEY_PRIVATE']
 EMAIL = os.environ['SUBMISSION_EMAIL']
 
 
-def send_mail(serialized_data, subject, type):
+def send_mail(serialized_data, subject, mail_type):
 
     mailjet = Client(auth=(API_KEY, API_SECRET), version='v3.1')
     name = serialized_data.get("name")
     phone = serialized_data.get("phone")
     email = serialized_data.get("email")
-    date = serialized_data.get("date")
+    mail_date = serialized_data.get("date")
     message = serialized_data.get("text")
     try:
         birthday_date = serialized_data.get("birthday_date").strftime("%d.%m")
     except AttributeError:
         birthday_date = datetime.now().strftime("%d.%m")
 
-
     html_body = {
-        "banket" : "<h4>Имя: {} </h4><h4>Телефон: {} </h4> <h4>Email: {} </h4> <br />дата заявки {}".format(name, phone, email, date),
-        "birthday": "<h4>Имя: {} </h4> <h4>Телефон: {} </h4> <h4>День Рождения: {} </h4> <br />дата заявки {}".format(name, phone, birthday_date, date),
-        "reserve": "<h4>Имя: {} </h4><h4>Телефон: {} </h4> <br />дата заявки {}".format(name, phone, date),
-        "feedback": "<h4>Имя: {} </h4><h4>Email: {} </h4>  <h4> Сообщение: </h4> {}".format(name, email, message)
-
+        "banket": "<h4>Имя: {} </h4><h4>Телефон: {} </h4> <h4>Email: {} </h4> <br />дата заявки {}"
+        .format(name, phone, email, mail_date),
+        "birthday": "<h4>Имя: {} </h4> <h4>Телефон: {} </h4> <h4>День Рождения: {} </h4> <br />дата заявки {}"
+        .format(name, phone, birthday_date, mail_date),
+        "reserve": "<h4>Имя: {} </h4><h4>Телефон: {} </h4> <br />дата заявки {}"
+        .format(name, phone, mail_date),
+        "feedback": "<h4>Имя: {} </h4><h4>Email: {} </h4>  <h4> Сообщение: </h4> {}"
+        .format(name, email, message)
     }
     data = {
         'Messages': [
@@ -43,10 +44,10 @@ def send_mail(serialized_data, subject, type):
                 ],
                 "Subject": subject,
                 "TextPart": "заказ",
-                "HTMLPart": html_body[type]
+                "HTMLPart": html_body[mail_type]
             }
         ]
     }
 
-    result = mailjet.send.create(data=data)
+    mailjet.send.create(data=data)
 
