2b9cd27f2972a790536c039a0a7000a9b9f6464b
Anton Kamynin <anton.kamynin@gmail.com>
2018-06-09 15:46:56 +0300

    feat: add models fields verbose and plural names (#4)
    refactor: organize models and admin sections code with
    PEP styleguide

--- REST/admin.py
+++ REST/admin.py
@@ -1,24 +1,29 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 from . import models
-# Register your models here.
 
 
 admin.site.register(models.Party, MarkdownxModelAdmin)
 admin.site.register(models.Places, MarkdownxModelAdmin)
-admin.site.register(models.Deals, MarkdownxModelAdmin)
+admin.site.register(models.Deals)
 admin.site.register(models.MenuCategories)
 admin.site.register(models.DishCategories)
 admin.site.register(models.Units)
 admin.site.register(models.Dish)
+
+
 @admin.register(models.Leads)
 class LeadsAdmin(admin.ModelAdmin):
     readonly_fields = ["name", "phone", "email", "date"]
+
 
 @admin.register(models.Birthdays)
 class LeadsAdmin(admin.ModelAdmin):
     readonly_fields = ["name", "phone", "email"]
 
+
 @admin.register(models.Bankets)
 class LeadsAdmin(admin.ModelAdmin):
     readonly_fields = ["name", "phone", "email"]
+
+
--- REST/models.py
+++ REST/models.py
@@ -1,74 +1,75 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from markdownx.models import MarkdownxField
 
 import datetime
 
 
-# Create your models here.
-
 class Party(models.Model):
-    name = models.CharField(max_length=255, verbose_name= 'Название вечерники')
-    date = models.DateField()
-    time_start = models.TimeField(default=datetime.time(22,00))
-    time_end = models.TimeField(default=datetime.time(6,00), blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name='Название')
+    date = models.DateField(verbose_name='Дата')
+    time_start = models.TimeField(default=datetime.time(22, 00), verbose_name='Начало мероприятия')
+    time_end = models.TimeField(default=datetime.time(6, 00), blank=True, null=True,
+                                verbose_name='Завершение мероприятия')
     description = MarkdownxField(verbose_name="Описание мероприятия")
-    place= models.ForeignKey('Places', on_delete=models.CASCADE)
-    repeat = models.BooleanField(default=False)
-    active = models.BooleanField(default = True)
-    on_main = models.BooleanField(default = True)
-    poster = models.ImageField(upload_to="posters", verbose_name="Изображение")
-    poster_alt = models.CharField(blank=True, max_length=255, verbose_name='Описание изображения')
-
+    place = models.ForeignKey('Places', on_delete=models.CASCADE,  verbose_name='Место проведения')
+    repeat = models.BooleanField(default=False,  verbose_name='Каджую неделю')
+    active = models.BooleanField(default=True,  verbose_name='Активно')
+    on_main = models.BooleanField(default=True,  verbose_name='Показывать на главной')
+    poster = models.ImageField(upload_to="posters", verbose_name='Афиша')
+    poster_alt = models.CharField(blank=True, max_length=255, verbose_name='Текст для афиши')
 
     def __str__(self):
-        return self.name
+        return '{} - {}'.format(self.date.strftime("%d.%m.%y"), self.name)
 
     class Meta:
-        verbose_name_plural = 'Вечерники'
+        verbose_name_plural = 'Мероприятия'
+        verbose_name = "Мероприятие"
+        ordering = ['date']
 
 
 class Places(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Место проведения мероприятия")
+    name = models.CharField(max_length=255, verbose_name="Название")
     location = models.CharField(max_length=255, default="пр-кт Испытателей 26/2", verbose_name="Адрес")
     open_hours = MarkdownxField(verbose_name="Режим работы")
     price = MarkdownxField(verbose_name="Цены")
-
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name_plural = 'Заведения'
+        verbose_name = "Заведение"
+
 
 class Deals(models.Model):
     name = models.CharField(verbose_name="Название акции", max_length=255)
+    order = models.IntegerField(verbose_name="Порядок отображения", default=0)
     description = MarkdownxField(verbose_name="Описание акции")
     date_start = models.DateField(verbose_name="Дата начала акции", blank=True, null=True)
     date_end = models.DateField(verbose_name="Дата окончания акции", blank=True, null=True)
     on_main = models.BooleanField(verbose_name="На главной странице")
     poster = models.ImageField(upload_to="deals", verbose_name="Изображение")
-    poster_alt = models.CharField(blank=True, max_length=255, verbose_name='Описание изображения' )
+    poster_alt = models.CharField(blank=True, max_length=255, verbose_name='Описание изображения')
 
     def __str__(self):
-
         return self.name
 
     class Meta:
         verbose_name_plural = "Акции"
         verbose_name = "Акция"
+        ordering = ["order"]
+
 
 class Units(models.Model):
-    unit = models.CharField(max_length=32)
-    description = models.CharField(max_length=64)
+    unit = models.CharField(max_length=32, verbose_name='Сокращение')
+    description = models.CharField(max_length=64,  verbose_name='Полное наименование')
 
     def __str__(self):
         return self.unit
 
     class Meta:
-        verbose_name_plural = "Единицы измерения"
+        verbose_name_plural = "Меню - Единицы измерения"
+        verbose_name = "Единица измерения"
 
 
 class MenuCategories(models.Model):
@@ -76,6 +77,11 @@ class MenuCategories(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = "Меню - Места реализации"
+        verbose_name = "Меню - Место реализации"
+
 
 class DishCategories(models.Model):
     menu_category = models.ForeignKey('MenuCategories', related_name='sub_category', on_delete=models.CASCADE)
@@ -85,75 +91,87 @@ class DishCategories(models.Model):
     def __str__(self):
         return '{} - {}'.format(self.menu_category, self.dish_category)
 
+    class Meta:
+        verbose_name_plural = "Меню - Категории блюд"
+        verbose_name = "Меню - Категория блюда"
+
 
 class Dish(models.Model):
-    name = models.CharField(max_length=255)
-    category = models.ForeignKey("DishCategories", related_name="dish", on_delete=models.CASCADE)
-    consist = models.TextField()
-    net = models.FloatField()
-    units = models.ForeignKey('Units', on_delete=models.CASCADE)
-    price = models.IntegerField()
-    iiko_id = models.IntegerField(blank=True, null= True)
-
-    class Meta:
-        verbose_name_plural = "Меню"
+    name = models.CharField(max_length=255, verbose_name="Название")
+    category = models.ForeignKey("DishCategories", related_name="dish", on_delete=models.CASCADE,
+                                 verbose_name="Категория")
+    consist = models.TextField(verbose_name="Состав")
+    net = models.FloatField(verbose_name="Вес/объем")
+    units = models.ForeignKey('Units', on_delete=models.CASCADE, verbose_name="Единица измерения")
+    price = models.IntegerField(verbose_name="Стоимость")
+    iiko_id = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return "{}. {}".format(self.category, self.name)
 
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Меню"
+        verbose_name = "Позиция"
+        ordering = ["category"]
+
 
 class Leads(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    reserve_date = models.DateField(blank=True, null = True)
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата заявки")
+    reserve_date = models.DateField(blank=True, null=True, verbose_name="Дата брони")
     name = models.CharField(max_length=256, verbose_name="Имя")
     phone = models.CharField(max_length=12, verbose_name="Телефон")
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, verbose_name="Email")
 
     def __str__(self):
         return "{} - {}".format(self.date.strftime("%d.%m.%y"), self.phone)
 
     class Meta:
         verbose_name_plural = "Заявки - бронирования"
+        verbose_name = "Заявка"
 
 
 class Birthdays(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    birthday_date = models.DateTimeField(blank=True, null = True)
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата заявки")
+    birthday_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата ДР")
     name = models.CharField(max_length=256, verbose_name="Имя")
     phone = models.CharField(max_length=12, verbose_name="Телефон")
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, verbose_name="Email")
 
     def __str__(self):
         return "{} - {}".format(self.date.strftime("%d.%m.%y"), self.phone)
 
     class Meta:
         verbose_name_plural = "Заявки - дни рождения"
+        verbose_name = "Заявка"
 
 
 class Bankets(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    banket_date = models.DateField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата заявки")
+    banket_date = models.DateField(blank=True, null=True, verbose_name="Дата банкета")
     name = models.CharField(max_length=256, verbose_name="Имя")
     phone = models.CharField(max_length=12, verbose_name="Телефон")
-    email = models.EmailField()
+    email = models.EmailField(verbose_name="Email")
 
     def __str__(self):
         return "{} - {}".format(self.date.strftime("%d.%m"), self.phone)
 
     class Meta:
         verbose_name_plural = "Заявки - банкеты"
+        verbose_name = "Заявка"
+
 
 class Feedbacks(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Дата заявки")
     name = models.CharField(max_length=256, verbose_name="Имя")
-    email = models.EmailField()
-    text = models.TextField()
+    email = models.EmailField(verbose_name="Email")
+    text = models.TextField(verbose_name="Сообщение")
 
     def __str__(self):
         return "Сообщение с контактной формы"
 
     class Meta:
         verbose_name_plural = "Заявки - обратная связь"
+        verbose_name = "Заявка"
